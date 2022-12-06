c632cd0e41a3754182254e2ac28a24bdab28b394
scare-destiny <zhenya.venger@gmail.com>
2022-12-06 10:22:07 +0200

    finished all exercises

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React from 'react'
 
-import CharacterEditor from './components/CharacterEditor';
-import Footer from './components/Footer';
+import CharacterEditor from './components/CharacterEditor'
+import Footer from './components/Footer'
 
 function App() {
-  return (
-    <>
-      <CharacterEditor />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<CharacterEditor />
+			<Footer />
+			<div className='fixedWrapper'></div>
+		</>
+	)
 }
 
-export default App;
+export default App
